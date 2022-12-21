5077a7b4941b126d6432bd8336236a6cd1bcd604
Firoz Hussain <87609163+Firoz1212@users.noreply.github.com>
2022-12-21 09:14:30 +0530

    Create Cubes sum.cpp

---
+++ Cubes sum.cpp
@@ -0,0 +1,44 @@
+/*
+Given a positive integer N, print the sum of cubes of 1st N natural numbers.
+
+Input Format
+
+First and only line of input contains a positive integer - N.
+
+Constraints
+
+1 <= N <= 102
+
+Output Format
+
+Print the sum of cubes of 1st N natural numbers.
+
+Sample Input 0
+
+4
+Sample Output 0
+
+100
+Explanation 0
+
+Self Explanatory
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N,s=0;
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        s+=i*i*i;
+    }
+    cout<<s<<endl;
+    
+    return 0;
+}
